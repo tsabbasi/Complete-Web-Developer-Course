1a2e3e6f2444a6f6f0304c6d30db25ce73f1f474
Tuba Abbasi <tsabbasi@gmail.com>
2016-07-10 12:34:41 -0400

    Contact Form completed

--- chatper-6-php/contact-form.php
+++ chatper-6-php/contact-form.php
@@ -1,48 +1,66 @@
 <?php
 
-  $error = "";
+    $error = ""; $successMessage = "";
 
-  if ($_POST) {  
+    if ($_POST) {
 
-    if (!$_POST["email"]) {
+        if (!$_POST["email"]) {
 
-      $error .= "An email address is required.<br>"
+            $error .= "An email address is required.<br>";
+
+        }
+
+        if (!$_POST["content"]) {
+
+            $error .= "The content field is required.<br>";
+
+        }
+
+        if (!$_POST["subject"]) {
+
+            $error .= "The subject is required.<br>";
+
+        }
+
+        if ($_POST['email'] && filter_var($_POST["email"], FILTER_VALIDATE_EMAIL) === false) {
+
+            $error .= "The email address is invalid.<br>";
+
+        }
+
+        if ($error != "") {
+
+            $error = '<div class="alert alert-danger" role="alert"><p>There were error(s) in your form:</p>' . $error . '</div>';
+
+        } else {
+
+            $emailTo = "tsabbasi@icloud.com";
+
+            $subject = $_POST['subject'];
+
+            $content = $_POST['content'];
+
+            $headers = "From: ".$_POST['email'];
+
+            if (mail($emailTo, $subject, $content, $headers)) {
+
+                $successMessage = '<div class="alert alert-success" role="alert">Your message was sent, we\'ll get back to you ASAP!</div>';
+
+
+            } else {
+
+                $error = '<div class="alert alert-danger" role="alert"><p><strong>Your message couldn\'t be sent - please try again later</div>';
+
+
+            }
+
+        }
+
+
 
     }
-
-    if (!$_POST["content"]) {
-
-      $error .= "The content field is required.<br>"
-
-    }
-
-    if (!$_POST["subject"]) {
-
-      $error .= "The subject field is required.<br>"
-
-    }
-
-    if (!$_POST["subject"]) {
-
-      $error .= "An email address is required.<br>"
-
-    }
-
-    if (filter_var($_POST["email"], FILTER_VALIDATE_EMAIL) === false) {
-
-      $error .= "The email address is invalid.<br>"
-
-    }
-
-    if (error != "") {
-
-          $error = '<div class="alert alert-danger" role="alert"><p><strong>There were error(s) in your form:</strong></p>' . $error . '</div>';
-    }
-
-  }
 
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -57,80 +75,74 @@
   </head>
   <body>
 
-    <div class="container">
+      <div class="container">
 
-      <h1>Get In Touch!</h1>
+    <h1>Get in touch!</h1>
+
+      <div id="error"><? echo $error.$successMessage; ?></div>
 
       <form method="post">
+  <fieldset class="form-group">
+    <label for="email">Email address</label>
+    <input type="email" class="form-control" id="email" name="email" placeholder="Enter email">
+    <small class="text-muted">We'll never share your email with anyone else.</small>
+  </fieldset>
+  <fieldset class="form-group">
+    <label for="subject">Subject</label>
+    <input type="text" class="form-control" id="subject" name="subject" >
+  </fieldset>
+  <fieldset class="form-group">
+    <label for="exampleTextarea">What would you like to ask us?</label>
+    <textarea class="form-control" id="content" name="content" rows="3"></textarea>
+  </fieldset>
+  <button type="submit" id="submit" class="btn btn-primary">Submit</button>
+</form>
 
-        <div id="error"><? echo $error; ?></div>
-
-        <fieldset class="form-group">
-          <label for="email">Email address</label>
-          <input type="email" class="form-control" id="email" name="email" placeholder="Enter your email address">
-          <small class="text-muted">We'll never share your email with anyone else.</small>
-        </fieldset>
-
-        <fieldset class="form-group">
-          <label for="subject">Subject</label>
-          <input type="text" class="form-control" id="subject" name="subject">
-        </fieldset>
-
-        <fieldset class="form-group">
-          <label for="content">What would you like to ask us?</label>
-          <textarea class="form-control" id="content" name="content" rows="3"></textarea>
-        </fieldset>
-
-        <button type="submit" id="submit" class="btn btn-primary">Submit</button>
-
-      </form>
-
-    </div>
+        </div>
 
     <!-- jQuery first, then Bootstrap JS. -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
 
+
     <script type="text/javascript">
 
-      // $("form").submit(function (e) {
+          $("form").submit(function(e) {
 
-      //   e.preventDefault();
+              var error = "";
 
-      //   var error = "";
+              if ($("#email").val() == "") {
 
-      //   if ($("#email").val() == "") {
+                  error += "<p>The email field is required.</p>"
 
-      //     error += "<p>The email field is required.</p>";
+              }
 
-      //   }
+              if ($("#subject").val() == "") {
 
-      //   if ($("#subject").val() == "") {
+                  error += "<b>The subject field is required.</p>"
 
-      //     error += "<p>The subject field is required.</p>";
+              }
 
-      //   }
+              if ($("#content").val() == "") {
 
-      //   if ($("#content").val() == "") {
+                  error += "<p>The content field is required.</p>"
 
-      //     error += "<p>The content field is required.</p>";
+              }
 
-      //   }
+              if (error != "") {
 
-      //   if (error != "") {
+                 $("#error").html('<div class="alert alert-danger" role="alert"><p><strong>There were error(s) in your form:</strong></p>' + error + '</div>');
 
-      //     $("#error").html('<div class="alert alert-danger" role="alert"><strong>There were error(s) in your form:</strong>' + error + '</div>');
+                  return false;
 
-      //   } else {
+              } else {
 
-      //     $("form").unbind('submit').submit();
+                  return true;
 
-      //   }
-
-
-
-      // });
+              }
+          })
 
     </script>
+
   </body>
 </html>
